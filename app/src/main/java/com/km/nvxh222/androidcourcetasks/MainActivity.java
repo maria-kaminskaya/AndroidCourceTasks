b007c24f7eab09d12ef98b214d6898f5b26577ba
Maria <km.nvxh222@gmail.com>
2020-07-14 12:49:18 +0300

    Initial commit

---
+++ app/src/main/java/com/km/nvxh222/androidcourcetasks/MainActivity.java
@@ -0,0 +1,14 @@
+package com.km.nvxh222.androidcourcetasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+    }
+}
